1f6e408ff54ea0b915443cf48828ab98bd0bb62d
zombah <zombah@gmail.com>
2014-08-10 00:28:11 +0400

    TEST: tegra: paz00: Revert ehci power settings
    Signed-off-by: zombah <zombah@gmail.com>

--- arch/arm/mach-tegra/board-paz00.c
+++ arch/arm/mach-tegra/board-paz00.c
@@ -321,7 +321,6 @@ static struct tegra_ehci_platform_data tegra_ehci_pdata[] = {
 			.phy_config = &utmi_phy_config[0],
 			.operating_mode = TEGRA_USB_OTG,
 			.power_down_on_bus_suspend = 1,
-			.hotplug = 1,
 			.default_enable = true,
 	},
 	[1] = {
@@ -334,7 +333,7 @@ static struct tegra_ehci_platform_data tegra_ehci_pdata[] = {
 	[2] = {
 			.phy_config = &utmi_phy_config[1],
 			.operating_mode = TEGRA_USB_HOST,
-			.power_down_on_bus_suspend = 1,
+			.power_down_on_bus_suspend = 0,
 			.default_enable = true,
 	},
 };
